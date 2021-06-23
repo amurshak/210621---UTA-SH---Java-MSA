509c51b63965896d92a8ff21ca4a594ff8f9c6fa
Alex Murshak <monicaward@Murshaks-Mac.local>
2021-06-23 14:33:50 -0400

    Hello World program

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    // This is a comment
+    /* 
+    This is a multiline comment
+    
+   
+    */
+     // THE MAIN METHOD This is special in Java
+    // ALways the starting point of the program
+
+    public static void main (String[] args) {
+        System.out.println("Hellow World!");
+    }
+}
